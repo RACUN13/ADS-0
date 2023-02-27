7acb2be9181b3c534f9f8dead8c9e7324314a1a6
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-02-27 17:33:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,15 @@
-// Copyright 2021 NNTU-CS
+#include <iostream>
 
-int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
-    //тест
+using namespace std;
+
+int main(void) {
+    int a, b, c;
+    cin >> a >> b;
+    while(b != 0) {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+
+cout << a;
 }
