3f3e8078070b2f5e4825b4bdf0e6679cb162e690
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-02-28 17:29:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-
-using namespace std;
-
 int gcd(int a, int b) {
   int c;
   while (b != 0) {
@@ -10,8 +6,4 @@ int gcd(int a, int b) {
     b = c;
   }
 return a;
-}
-
-int main() {
-  cout << gcd(40, 60) << endl;
 }
